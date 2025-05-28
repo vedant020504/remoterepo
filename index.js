e68bb8d0f169353d2049751c4db56c5d520c1b2a
vedant020504 <vedantsingh29303@gmail.com>
2025-05-28 15:26:59 +0530

    ok
    ok

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Route for home page
+app.get('/', (req, res) => {
+    res.render('index', { message: 'Hello World' });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
